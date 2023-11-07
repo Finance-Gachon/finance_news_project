317b81d2da6b99776c18234716525aa409998df8
GinToFim <shlf6673@gmail.comm>
2023-11-07 13:17:57 +0900

    Feat: CORS middleware setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
-from typing import Union
+from typing import Union, Optional
 from fastapi import FastAPI, Response, Request, Depends
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
 import json
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
@@ -17,12 +19,30 @@ def get_db():
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     result = json.dumps({"message": "Hello World"})
-    return Response(result, media_type="application/json")
+    # return Response(result, media_type="application/json")
+    return {"message": "Hello World"}
+    # return {"hello goodbye"}
 
 @app.get("/news/", response_model=list[schemas.News_crwal])
 def read_news(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     news = crud.get_news(db)
     return news
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
