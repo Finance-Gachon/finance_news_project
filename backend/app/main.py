faaf1ef8d064797d1fc59d21d3a242a9ed3c6a7c
GinToFim <shlf6673@gmail.comm>
2023-11-14 13:16:51 +0900

    Feat: GET search api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from typing import Union, Optional
-from fastapi import FastAPI, Response, Request, Depends
+from fastapi import FastAPI, Response, Request, Depends, Query, Header
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,6 +8,21 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+
+from fastapi.logger import logger
+# ... other imports
+import logging
+
+company_searching = [
+    {
+        "company_name": "삼성전자",
+        "item": "Read a book."
+    },
+    {
+        "company_name": "LG에너지솔루션",
+        "item": "Read a book."
+    }
+]
 
 # Dependency
 def get_db():
@@ -18,9 +33,11 @@ def get_db():
         db.close()
 
 app = FastAPI()
+logger = logging.getLogger("uvicorn.error")
 
 origins = [
     "http://localhost:3000",
+    "localhost:3000"
 ]
 
 app.add_middleware(
@@ -34,15 +51,29 @@ app.add_middleware(
 @app.get("/")
 async def root():
     result = json.dumps({"message": "Hello World"})
+    search='삼성전자'
+    logger.info(search)
+    logger.info(msg="Hello!")
     # return Response(result, media_type="application/json")
     return {"message": "Hello World"}
     # return {"hello goodbye"}
 
-@app.get("/news/", response_model=list[schemas.News_crwal])
-def read_news(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    news = crud.get_news(db)
-    return news
+@app.post("/search")
+async def request_search(search:dict) -> dict:
+    logger.info("Hello!")
+    logger.info(search)
+    # return {'data': 'good'}
+    return search
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search/{company_name}&{start_date}&{end_date}")
+async def request_search(company_name, start_date, end_date):
+    logger.info(company_name)
+    logger.info(start_date)
+    logger.info(end_date)
+    
+    new_companys = []
+    for one in company_searching:
+        if one["company_name"] == company_name:
+            new_companys.append(one)
+
+    return {"data" : new_companys}
